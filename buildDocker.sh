429c954d4331121e3af7a9ffc2fb47c2be16442c
fabry00 <fabry_00@hotmail.com>
2021-06-17 10:51:27 +0200

    test

--- buildDocker.sh
+++ buildDocker.sh
@@ -3,4 +3,5 @@
 set -x
 docker --version
 
-docker build --no-cache -f src/main/docker/Dockerfile.multistage -t quarkus-quickstart/rest-client-quickstart .
+#docker build --no-cache -f src/main/docker/Dockerfile.multistage -t quarkus-quickstart/rest-client-quickstart .
+docker build -f src/main/docker/Dockerfile.multistage -t quarkus-quickstart/rest-client-quickstart .
